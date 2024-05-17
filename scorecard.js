21a1507dc9d9763f3b5530bc09aa1cc846f84080
Pokesalot <athankinson@gmail.com>
2024-05-17 11:01:09 -0400

    It's alive!!!!!!?

--- scorecard.js
+++ scorecard.js
@@ -1,7 +1,8 @@
 //Generate Card
 let card = document.getElementById("Scoreboard");
 let rows = ["#ed152f", "#fdbd0d","#0e68b4","#0ca950"];
-let scores = [0,0,0,0];
+let scores = [0,0,0,0,0];
+let scoreVals = [0,1,3,6,10,15,21,28,36,45,55,66,78]
 
 let newHTML = "";
 
@@ -14,7 +15,7 @@ for(let row = 0; row<rows.length; row++){
             if(number < 12){
                 newHTML += `<td id="${row}${number}" onClick="score(${row},${number-2})">${number}</td>`;
             }else{
-                newHTML += `<td id="Lock${row}" style="background-color: gray;"></td>`
+                newHTML += `<td id="${row}${number}" onClick="score(${row},${number-2})" style="background-color: gray;"></td>`
             }
         }
     }else{
@@ -23,39 +24,69 @@ for(let row = 0; row<rows.length; row++){
             if(number < 12){
                 newHTML += `<td id="${row}${number}" onClick="score(${row},${number-2})">${14-number}</td>`;
             }else{
-                newHTML += `<td id="Lock${row}" style="background-color: gray;"></td>`
+                newHTML += `<td id="${row}${number}" onClick="score(${row},${number-2})" style="background-color: gray;"></td>`
             }
         }
     }
 
-    newHTML += `<td>🔓</td></tr>\n`
+    newHTML += `<td id="${row}13" onclick="score(${row},11)">🔓</td></tr>\n`
 }
 
 card.innerHTML = newHTML;
 
 
 function score(row, cell){//row is [0,3], cell is [0,11]
-    if(cell == 10){
+
+    let curcell = document.getElementById(`${row}${cell+2}`);
+    if(curcell.innerText == "" || curcell.innerText == "X"){console.log("already scored");return};
+
+    if(cell == 11){
+
+    }else if(cell == 10){
         scores[row]+= 2;
+        let getLock = document.getElementById(`${row}13`)
+        getLock.innerText = "X";
     }else{
-        let curcell = document.getElementById(`${row}${cell+2}`);
-        if(curcell.innerText == ""){return};
-
-
         scores[row]++;
-
-        curcell.innerText = "X";
-        for(let i=cell+1; i>=2; i--){
-            let cur = document.getElementById(`${row}${i}`)
-            cur.innerText=="X"?i=0:cur.innerText = "";
-        }
-
         if(scores[row] >= 5){
-            let unlock = document.getElementById(`Lock${row}`);
+            let unlock = document.getElementById(`${row}12`);
             unlock.style.backgroundColor = rows[row];
             unlock.innerText = row>=2?"2":"12";
             unlock.onclick = function(){score(row,10);};
         }
     }
-    
+
+    curcell.innerText = "X";
+    for(let i=cell+1; i>=2; i--){
+        let cur = document.getElementById(`${row}${i}`)
+        cur.innerText=="X"?i=0:cur.innerText = "";
+    }
+    showScore();
+}
+
+function penalty(){
+    if(scores[4] == 4){return}
+    let penalties = document.getElementById("penaltyRow");
+    let curPen = penalties.getElementsByTagName("td")[scores[4]];
+    curPen.innerText = "X";
+    curPen.style.color = "red";
+
+    scores[4]++;
+    showScore();
+}
+
+function showScore(){
+    let totalScore = 0;
+    for(let i=0;i<4;i++){
+        let curScore = document.getElementById(`score${i}`);
+        curScore.innerText = scoreVals[scores[i]];
+        totalScore += scoreVals[scores[i]];
+    }
+
+    let curScore = document.getElementById(`score4`);
+    curScore.innerText = 5*scores[4];
+    totalScore -= 5*scores[4];
+
+    curScore = document.getElementById(`total`);
+    curScore.innerText = totalScore;
 }
