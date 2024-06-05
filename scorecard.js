2eda9eda831d6f519f7ee469d8f962ab83c6f471
pokesalot <athankinson@gmail.com>
2024-06-05 01:15:36 -0400

    (Add) Undo Button!

--- scorecard.js
+++ scorecard.js
@@ -2,7 +2,8 @@
 let card = document.getElementById("Scoreboard");
 let rows = ["#ed152f", "#fdbd0d","#0e68b4","#0ca950"];
 let scores = [0,0,0,0,0];
-let scoreVals = [0,1,3,6,10,15,21,28,36,45,55,66,78]
+let scoreVals = [0,1,3,6,10,15,21,28,36,45,55,66,78];
+let moves = [];
 
 let newHTML = "";
 
@@ -13,18 +14,18 @@ for(let row = 0; row<rows.length; row++){
         //red and yellow, low to high
         for(let number = 2; number <= 12; number++){
             if(number < 12){
-                newHTML += `<td id="${row}${number}" onClick="score(${row},${number-2})">${number}</td>`;
+                newHTML += `<td id="${row}${number}" onClick="pushScore(${row},${number-2})">${number}</td>`;
             }else{
-                newHTML += `<td id="${row}${number}" onClick="score(${row},${number-2})" style="background-color: gray;"></td>`
+                newHTML += `<td id="${row}${number}" onClick="pushScore(${row},${number-2})" style="background-color: gray;"></td>`
             }
         }
     }else{
         //blue and green, high to low
         for(let number = 2; number <= 12; number++){
             if(number < 12){
-                newHTML += `<td id="${row}${number}" onClick="score(${row},${number-2})">${14-number}</td>`;
+                newHTML += `<td id="${row}${number}" onClick="pushScore(${row},${number-2})">${14-number}</td>`;
             }else{
-                newHTML += `<td id="${row}${number}" onClick="score(${row},${number-2})" style="background-color: gray;"></td>`
+                newHTML += `<td id="${row}${number}" onClick="pushScore(${row},${number-2})" style="background-color: gray;"></td>`
             }
         }
     }
@@ -33,6 +34,66 @@ for(let row = 0; row<rows.length; row++){
 }
 
 card.innerHTML = newHTML;
+
+function pushScore(row, cell){
+    moves.push([row,cell]);
+    displayScores();
+}
+
+function popScore(){
+    if(moves.length >= 1){
+        moves.pop();
+    }
+    displayScores();
+}
+
+function displayScores(){
+    scores = [0,0,0,0,0];
+    for(let row=0;row<=1;row++){
+        for(let cell=2;cell<=11;cell++){
+            let curCell = document.getElementById(`${row}${cell}`);
+            curCell.innerText = cell;
+        }
+        let curCell = document.getElementById(`${row}12`);
+        curCell.style.backgroundColor = "gray";
+        curCell.innerText = "";
+
+        curCell = document.getElementById(`${row}13`);
+        curCell.innerText = "🔓";
+    }
+    for(let row=2;row<=3;row++){
+        for(let cell=2;cell<=11;cell++){
+            let curCell = document.getElementById(`${row}${cell}`);
+            curCell.innerText = 14-cell;
+        }
+        let curCell = document.getElementById(`${row}12`);
+        curCell.style.backgroundColor = "gray";
+        curCell.innerText = "";
+        
+        curCell = document.getElementById(`${row}13`);
+        curCell.innerText = "🔓";
+    }
+    let penalties = document.getElementById("penaltyRow");
+    for(let curPen=0; curPen<4; curPen++){
+        penalties.getElementsByTagName("td")[curPen].innerText = "1";
+        penalties.getElementsByTagName("td")[curPen].style.color = "white";
+    }
+
+    for(let i=0; i<moves.length; i++){
+        if(moves[i][0] == 5){
+            penalty();
+        }else{
+            score(moves[i][0], moves[i][1]);
+        }
+        
+    }
+    if(moves.length >= 10){
+        document.getElementById("spaceBuffer").innerText = "";
+    }else{
+        document.getElementById("spaceBuffer").innerText = "0";
+    }
+    showScore();
+}
 
 
 function score(row, cell){//row is [0,3], cell is [0,11]
@@ -61,7 +122,6 @@ function score(row, cell){//row is [0,3], cell is [0,11]
         let cur = document.getElementById(`${row}${i}`)
         cur.innerText=="X"?i=0:cur.innerText = "";
     }
-    showScore();
 }
 
 function penalty(){
